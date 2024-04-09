0cc051fa7d26330f1fa441fae4d3faa71ed0b843
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-04-09 12:35:00 +0200

    feat: Add Header.jsx component
    Header component for greeting a user

---
+++ pokemem/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className={"header"}>
+            <h1>Welcome to PokeMem!</h1>
+            <p>Memory card game with your favorite characters from childhood</p>
+        </div>
+    )
+
+}
+
+export default Header
